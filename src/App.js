6eb43be5507727218767314643ae0291bf43c6e0
Ananta Rai <anantarai365@gmail.com>
2023-10-15 22:45:04 +0545

    tailwind css added & design changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 
 const App = () => {
 	const todoRef = React.useRef(null);
@@ -17,8 +18,10 @@ const App = () => {
 		}
 	};
 	return (
-		<div>
-			<h1>Todos</h1>
+		<div className="w-3/4 mx-auto">
+			<h1 className="text-slate-900 font-extrabold text-3xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white mb-8">
+				Todos
+			</h1>
 			<InputField onFormSubmit={onFormSubmit} ref={todoRef} />
 			<TodoList todos={todos} />
 		</div>
@@ -32,6 +35,7 @@ const InputField = React.forwardRef((props, ref) => {
 			<input
 				type="text"
 				name="task"
+				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
 				placeholder="What task needs to be completed?"
 				ref={ref}
 			/>
@@ -41,14 +45,25 @@ const InputField = React.forwardRef((props, ref) => {
 
 const TodoList = ({ todos }) => {
 	return (
-		<ol>
+		<ol className="w-1/2 mx-auto mt-8">
 			{todos.map((todo, idx) => {
 				return (
-					<li key={`${todo.title}-${idx}`}>
-						{todo.title} <br />
-						<strong>Status:</strong>
-						{todo.isCompleted ? "Completed" : "Pending"}
-						<hr />
+					<li
+						key={`${todo.title}-${idx}`}
+						className="h-16 border-b border-gray-200 dark:border-gray-800 p-8"
+					>
+						<input
+							id={`${todo.title}-${idx}`}
+							type="checkbox"
+							defaultValue=""
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-0 dark:bg-gray-700 dark:border-gray-600"
+						/>
+						<label
+							htmlFor={`${todo.title}-${idx}`}
+							className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+						>
+							{todo.title}
+						</label>
 					</li>
 				);
 			})}
