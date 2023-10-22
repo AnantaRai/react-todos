b370d58e2e64271884e7354925733f058bde1de5
Ananta Rai <anantarai365@gmail.com>
2023-10-22 22:42:33 +0545

    edit mode wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { forwardRef, useRef } from "react";
 import "./index.css";
 import Todos from "./todos.json";
 import { config } from "./config";
 
 const App = () => {
-	const todoRef = React.useRef(null);
+	const todoRef = useRef(null);
+	const editTodoRef = useRef(null);
+
 	const [todos, setTodos] = React.useState(
 		config.useJsonFileForTodos ? Todos.todoList : []
 	);
@@ -16,7 +18,7 @@ const App = () => {
 		if (todo.trim().length > 0) {
 			setTodos((prevTodo) => [
 				...prevTodo,
-				{ title: todo, isCompleted: false },
+				{ title: todo, isCompleted: false, isEditing: false },
 			]);
 			todoRef.current.value = null;
 		}
@@ -31,25 +33,56 @@ const App = () => {
 			);
 		}
 	};
+
+	const handleEditMode = (todoIdx) => {
+		if (todos[todoIdx] !== "undefined") {
+			setTodos(
+				todos.map((todo, idx) =>
+					idx === todoIdx ? { ...todo, isEditing: true } : todo
+				)
+			);
+		}
+	};
+
+	const handleFocusOut = (todoIdx) => {
+		if (todos[todoIdx] !== "undefined") {
+			setTodos(
+				todos.map((todo, idx) =>
+					idx === todoIdx ? { ...todo, isEditing: false } : todo
+				)
+			);
+		}
+	};
+
+	const handleKeyDown = (todoIdx, e) => {
+		if (e.key === "Escape") {
+			handleFocusOut(todoIdx);
+		}
+	};
 	return (
-		<div className="w-3/4 mx-auto">
-			<h1 className="text-slate-900 font-extrabold text-3xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white mb-8">
-				Todos
-			</h1>
+		<div className="container">
+			<h1 className="todo-title">Todos</h1>
 			<InputField onFormSubmit={onFormSubmit} ref={todoRef} />
-			<TodoList todos={todos} handleCheckboxToggle={handleCheckboxToggle} />
+			<TodoList
+				todos={todos}
+				handleCheckboxToggle={handleCheckboxToggle}
+				ref={editTodoRef}
+				handleEditMode={handleEditMode}
+				handleFocusOut={handleFocusOut}
+				handleKeyDown={handleKeyDown}
+			/>
 		</div>
 	);
 };
 
-const InputField = React.forwardRef((props, ref) => {
+const InputField = forwardRef((props, ref) => {
 	const { onFormSubmit } = props;
 	return (
 		<form onSubmit={onFormSubmit}>
 			<input
 				type="text"
 				name="task"
-				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
+				className="todo-input"
 				placeholder="What task needs to be completed?"
 				ref={ref}
 			/>
@@ -57,35 +90,78 @@ const InputField = React.forwardRef((props, ref) => {
 	);
 });
 
-const TodoList = ({ todos, handleCheckboxToggle }) => {
+const TodoList = forwardRef((props, ref) => {
+	const {
+		todos,
+		handleCheckboxToggle,
+		handleEditMode,
+		handleFocusOut,
+		handleKeyDown,
+	} = props;
 	return (
-		<ol className="w-1/2 mx-auto mt-8">
+		<ol className="todo-list">
 			{todos.map((todo, todoIdx) => {
 				return (
-					<li
-						key={`${todo.title}-${todoIdx}`}
-						className="h-16 border-b border-gray-200 dark:border-gray-800 p-8"
-					>
-						<input
-							id={`${todo.title}-${todoIdx}`}
-							type="checkbox"
-							defaultValue=""
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-0 dark:bg-gray-700 dark:border-gray-600"
-							onChange={() => handleCheckboxToggle(todoIdx)}
-						/>
-						<label
-							htmlFor={`${todo.title}-${todoIdx}`}
-							className={`ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 ${
-								todo.isCompleted ? "line-through" : ""
-							}`}
-						>
-							{todo.title}
-						</label>
+					<li key={`${todo.title}-${todoIdx}`} className="todo-list-item">
+						{!todo.isEditing && (
+							<div className="flex justify-between items-center">
+								<div>
+									<input
+										id={`${todo.title}-${todoIdx}`}
+										type="checkbox"
+										className="todo-check-box"
+										onChange={() => handleCheckboxToggle(todoIdx)}
+									/>
+									<label
+										htmlFor={`${todo.title}-${todoIdx}`}
+										className={`todo-check-box-label ${
+											todo.isCompleted ? "line-through" : ""
+										}`}
+									>
+										{todo.title}
+									</label>
+								</div>
+								<div>
+									<span
+										className="hover:cursor-pointer"
+										onClick={() => handleEditMode(todoIdx)}
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="w-5 h-5"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+											/>
+										</svg>
+									</span>
+								</div>
+							</div>
+						)}
+
+						{todo.isEditing && (
+							<input
+								type="text"
+								name="task"
+								ref={ref}
+								className="todo-edit-input"
+								defaultValue={todo.title}
+								onBlur={() => handleFocusOut(todoIdx)}
+								autoFocus
+								onKeyDown={(e) => handleKeyDown(todoIdx, e)}
+							/>
+						)}
 					</li>
 				);
 			})}
 		</ol>
 	);
-};
+});
 
 export default App;
